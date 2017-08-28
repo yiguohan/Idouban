4ea2c08bfba4dd70e6f5bae790468a2fd55cc54c
yiguohan <yiguohan@gmail.com>
2017-08-29 00:01:19 +0800

    MainActivity

--- app/src/main/java/com/yiguohan/idouban/viewImpl/MainActivity.java
+++ app/src/main/java/com/yiguohan/idouban/viewImpl/MainActivity.java
@@ -4,12 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yiguohan.idouban.R;
+import com.yiguohan.idouban.base.BaseActivity;
+import com.yiguohan.idouban.base.BasePresenter;
+import com.yiguohan.idouban.bean.top250.Root;
+import com.yiguohan.idouban.viewinterface.film.IGetTop250View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements IGetTop250View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void getTop250Success(Root root, boolean isLoadMore) {
+
+    }
+
+    @Override
+    public void getDataFail() {
+
+    }
+
+    @Override
+    public String setActName() {
+        return null;
     }
 }
